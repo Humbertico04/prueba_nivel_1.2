b5299349316ace71d32b2ccfad754dd965288013
Humbertico04 <humbertico2004@gmail.com>
2023-03-12 19:51:37 +0100

    eliminar termino

--- menu.py
+++ menu.py
@@ -15,8 +15,9 @@ def iniciar():
         print(f"[2] Añadir polinomio (p{contador})")
         print("[3] Modificar término   ")
         print("[4] Añadir término      ")
-        print("[4] Borrar término      ")
-        print("[6] Cerrar el Gestor    ")
+        print("[5] Borrar término      ")
+        print("[6] Borrar polinomio    ")
+        print("[7] Cerrar el Gestor    ")
         print("========================")
 
         opcion = input("> ")
@@ -49,8 +50,9 @@ def iniciar():
             contador += 1
 
         elif opcion == '3':
-            print("Modificando polinomio...\n")
-            polinomios = helpers.leer_numero(1, contador-1, "Que polinomio desea modifcar? (Introduzca solo el número del polinomio 1, 2, 3, etc.):", mensaje_error="No existe un polinomio con el número ingresado.\n")
+            print("Modificando término...\n")
+            polinomios = helpers.leer_numero(1, contador-1, "Que polinomio desea modifcar? (Introduzca solo el número del polinomio 1, 2, 3, etc.):", 
+                                             mensaje_error="No existe un polinomio con el número ingresado.\n")
             if polinomios == None:
                 input("Presiona ENTER para continuar...")
                 continue
@@ -59,21 +61,48 @@ def iniciar():
             if polinomio[f"p{polinomios}"].existe_termino(termino) == True:
                 polinomio[f"p{polinomios}"].modificar_termino(termino, helpers.leer_numero(-1000, 1000, "Nuevo coeficiente del término: "))
                 helpers.limpiar_pantalla()
-                print("Polinomio modificado correctamente.")
+                print(f"p{polinomios} modificado correctamente.")
             else:
                 print("No existe un término con el exponente ingresado.")
-                
 
+        elif opcion == '4':
+            print("Añadiendo termino...\n")
+            polinomios = helpers.leer_numero(1, contador-1, "Que polinomio desea modifcar? (Introduzca solo el número del polinomio 1, 2, 3, etc.):", 
+                                             mensaje_error="No existe un polinomio con el número ingresado.\n")
+            if polinomios == None:
+                input("Presiona ENTER para continuar...")
+                continue
+            helpers.limpiar_pantalla()
+            while True:
+                if helpers.pedir_entrada_si_o_no(mensaje="Desea añadir otro término al polinomio p{}?".format(polinomios)) == False:
+                    break
+                else:
+                    exp = helpers.leer_numero(0, 1000, "Exponente del término: ")
+                    if polinomio[f"p{polinomios}"].existe_termino(exp) == True:
+                        helpers.limpiar_pantalla()
+                        print("Ya existe un término con el exponente ingresado.\n")
+                    else:
+                        polinomio[f"p{polinomios}"].agregar_termino(exp, helpers.leer_numero(-1000, 1000, "Coeficiente del término: "))
+                        helpers.limpiar_pantalla()
+            print(f"p{polinomios} modificado correctamente.")
 
+        elif opcion == '5':
+            print("Borrando un término...\n")
+            polinomios = helpers.leer_numero(1, contador-1, "Que polinomio desea modifcar? (Introduzca solo el número del polinomio 1, 2, 3, etc.):",
+                                                mensaje_error="No existe un polinomio con el número ingresado.\n")
+            if polinomios == None:
+                input("Presiona ENTER para continuar...")
+                continue
+            helpers.limpiar_pantalla()
+            termino = helpers.leer_numero(0, 1000, "Exponente del término a eliminar: ")
+            if polinomio[f"p{polinomios}"].existe_termino(termino) == True:
+                polinomio[f"p{polinomios}"].eliminar_termino(termino)
+                helpers.limpiar_pantalla()
+                print(f"Término eliminado correctamente.")
+            else:
+                print("No existe un término con el exponente ingresado.")
 
-
-        # elif opcion == '5':
-        #     print("Borrando un cliente...\n")
-        #     dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
-        #     print("Cliente borrado correctamente.") if db.Clientes.borrar(
-        #         dni) else print("Cliente no encontrado.")
-
-        elif opcion == '6':
+        elif opcion == '7':
             print("Saliendo...\n")
             break
 
